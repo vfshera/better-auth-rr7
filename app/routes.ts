db512e2c460af0204346a1fb1e94edb3912ca154
Franklin Shera <fshera96@gmail.com>
2024-12-16 02:27:43 +0300

    add Remix Flat Routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import type { RouteConfig } from "@react-router/dev/routes";
+import { remixConfigRoutes } from "@react-router/remix-config-routes-adapter";
+import { flatRoutes } from "remix-flat-routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default remixConfigRoutes((defineRoutes) => {
+  return flatRoutes("routes", defineRoutes);
+}) satisfies RouteConfig;
