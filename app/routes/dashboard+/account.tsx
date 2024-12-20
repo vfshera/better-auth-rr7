cbc2ab082cea730b3d0f1795621851e2910760ec
Franklin Shera <fshera96@gmail.com>
2024-12-20 04:32:00 +0300

    add account page

---
+++ app/routes/dashboard+/account.tsx
@@ -0,0 +1,27 @@
+import { requireAuth } from "~/.server/auth/utils";
+import type { Route } from "./+types/account";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const { user } = await requireAuth(request);
+
+  return { user };
+}
+
+export default function Account({
+  loaderData: { user },
+}: Route.ComponentProps) {
+  return (
+    <div className="p-5 space-y-5">
+      <h1 className="text-2xl font-semibold">Account</h1>
+
+      <section>
+        <p>
+          <strong>Name:</strong> {user.name}
+        </p>
+        <p>
+          <strong>Email:</strong> {user.email}
+        </p>
+      </section>
+    </div>
+  );
+}
